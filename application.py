0466fd0202e9a66b62e7282fb3d9a96af5309a27
golfen <golfen.guo@daocloud.io>
2015-03-29 22:16:37 +0800

    initial commit

---
+++ application.py
@@ -0,0 +1,29 @@
+import os
+import flask
+import redis
+
+application = flask.Flask(__name__)
+application.debug = True
+
+@application.route('/')
+def hello_world():
+  storage = Storage()
+  storage.populate()
+  score = storage.score()
+  return "Hello world, %s!" % score
+
+class Storage():
+  def __init__(self):
+    self.redis = redis.Redis(
+      host=os.getenv('REDIS_PORT_6379_TCP_ADDR', 'localhost'),
+      port=int(os.getenv('REDIS_PORT_6379_TCP_PORT', '6379')),
+      password=os.getenv('REDIS_PASSWORD', ''))
+
+  def populate(self):
+    self.redis.set('score', '1234')
+
+  def score(self):
+    return self.redis.get('score')
+
+if __name__ == "__main__":
+  application.run(host='0.0.0.0', port=3000)
